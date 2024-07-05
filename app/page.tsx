4d96c9e660bfd90ea6554b58c9193a306b28a63b
chrisvasqm <christianvasquezmelo@gmail.com>
2024-07-04 20:55:32 -0400

    Adjust content height

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Flex, Heading } from '@radix-ui/themes';
 export default function Home() {
   return (
     <main>
-      <Flex style={{ height: "100vh" }} justify={'center'} align={'center'}>
+      <Flex style={{ height: "90vh" }} justify={'center'} align={'center'}>
         <Heading as='h1'>Home page</Heading>
       </Flex>
     </main>
