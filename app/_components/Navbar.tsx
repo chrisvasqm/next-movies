61f4c99265a1dcd61dceb4d6e38038865c71ce94
chrisvasqm <christianvasquezmelo@gmail.com>
2024-07-04 21:42:41 -0400

    Add new movie

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,12 +1,17 @@
-import { Button, Flex } from '@radix-ui/themes';
-import Link from 'next/link'
-import React from 'react'
+import { PlusIcon } from '@radix-ui/react-icons';
+import { Flex, IconButton } from '@radix-ui/themes';
+import Link from 'next/link';
 
 function Navbar() {
   return (
     <nav style={{ boxShadow: '0 0 3px 1px' }}>
-      <Flex style={{ paddingLeft: '10px', height: '60px' }} direction={'row'} align={'center'}>
+      <Flex style={{ paddingLeft: '10px', height: '60px' }} direction={'row'} align={'center'} gap='5'>
         <h3><Link href={'/'}>Moveez</Link></h3>
+        <Link href={'/movies/new'}>
+          <IconButton>
+            <PlusIcon width="18" height="18" />
+          </IconButton>
+        </Link>
       </Flex>
     </nav>
   )
