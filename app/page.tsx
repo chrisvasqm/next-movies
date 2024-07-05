7ceac9bb7522c5233b396939f0605b9fa5f3d4d3
chrisvasqm <christianvasquezmelo@gmail.com>
2024-07-04 21:47:27 -0400

    Update movie titles

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import { Box } from '@radix-ui/themes';
 import MovieList from './MovieList';
 
 const movies = [
-  { id: 1, title: "Movie 1", description: "A long description...", price: 9.99 },
-  { id: 2, title: "Movie 2", description: "A long description...", price: 9.99 },
-  { id: 3, title: "Movie 3", description: "A long description...", price: 9.99 },
-  { id: 4, title: "Movie 4", description: "A long description...", price: 9.99 },
-  { id: 5, title: "Movie 5", description: "A long description...", price: 9.99 },
-  { id: 6, title: "Movie 6", description: "A long description...", price: 9.99 }
+  { id: 1, title: 'Transformer 3', description: 'A long description...', price: 9.99 },
+  { id: 2, title: 'Lord of the Rings', description: 'A long description...', price: 9.99 },
+  { id: 3, title: 'Star Wars', description: 'A long description...', price: 9.99 },
+  { id: 4, title: 'Step Up 4', description: 'A long description...', price: 9.99 },
+  { id: 5, title: 'Cars', description: 'A long description...', price: 9.99 },
+  { id: 6, title: 'Moana', description: 'A long description...', price: 9.99 }
 ]
 
 export default function Home() {
