fb60dff818b036d90aabd0bfbc7fa0ec5074d7cb
chrisvasqm <christianvasquezmelo@gmail.com>
2024-07-04 20:53:24 -0400

    Remove catalog page

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -7,12 +7,6 @@ function Navbar() {
     <nav style={{ boxShadow: '0 0 3px 1px' }}>
       <Flex style={{ paddingLeft: '10px', height: '60px' }} direction={'row'} align={'center'}>
         <h3><Link href={'/'}>Moveez</Link></h3>
-
-        <Link href={'/catalog'} style={{ height: '100%' }}>
-          <Button style={{ marginLeft: 20, fontWeight: 'bold', height: '100%' }} variant='ghost'>
-            Catalog
-          </Button>
-        </Link>
       </Flex>
     </nav>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Flex, Heading } from '@radix-ui/themes';
 
 export default function Home() {
   return (
     <main>
-      <p>Content area</p>
+      <Flex style={{ height: "100vh" }} justify={'center'} align={'center'}>
+        <Heading as='h1'>Home page</Heading>
+      </Flex>
     </main>
   );
 }
