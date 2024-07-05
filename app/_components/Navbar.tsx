2e2794a8bb865a80e9cd396402956fad58ed65b0
chrisvasqm <christianvasquezmelo@gmail.com>
2024-07-04 20:47:52 -0400

    Improve Navbar styling

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,10 +1,19 @@
+import { Button, Flex } from '@radix-ui/themes';
 import Link from 'next/link'
 import React from 'react'
 
 function Navbar() {
   return (
-    <nav className='nav'>
-      <h3><Link href={"/"}>Moveez</Link></h3>
+    <nav style={{ boxShadow: '0 0 3px 1px' }}>
+      <Flex style={{ paddingLeft: '10px', height: '60px' }} direction={'row'} align={'center'}>
+        <h3><Link href={'/'}>Moveez</Link></h3>
+
+        <Link href={'/catalog'} style={{ height: '100%' }}>
+          <Button style={{ marginLeft: 20, fontWeight: 'bold', height: '100%' }} variant='ghost'>
+            Catalog
+          </Button>
+        </Link>
+      </Flex>
     </nav>
   )
 }
