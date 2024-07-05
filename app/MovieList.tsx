955c1d688de3cc34bcaa715321dbda3c9a65fe63
chrisvasqm <christianvasquezmelo@gmail.com>
2024-07-04 22:16:14 -0400

    Refactor movie list with endpoint

--- app/MovieList.tsx
+++ app/MovieList.tsx
@@ -1,16 +1,37 @@
-import { Grid } from '@radix-ui/themes'
-import MovieCard from './MovieCard'
-import Movie from './_models/Movie'
+'use client'
 
-interface Props {
-  movies: Movie[]
-}
+import { Grid } from '@radix-ui/themes';
+import { useEffect, useState } from 'react';
+import MovieCard from './MovieCard';
+import Movie from './_models/Movie';
 
-function MovieList({ movies }: Props) {
+function MovieList() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await fetch('/api/movies')
+      const movies = await response.json()
+      setLoading(false)
+      setMovies(movies)
+    }
+
+    fetchMovies()
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>
+
   return (
-    <Grid columns={{ initial: '1', xs: '2', sm: '2', md: '3', lg: '4' }} gap='3'>
-      {movies.map(movie => <MovieCard key={movie.id} movie={movie} />)}
-    </Grid>
+    <>
+      {movies.length === 0 && <p>Catalog is empty</p>}
+
+      {movies &&
+        <Grid columns={{ initial: '1', xs: '2', sm: '2', md: '3', lg: '4' }} gap='3'>
+          {movies.map(movie => <MovieCard key={movie.id} movie={movie} />)}
+        </Grid>
+      }
+    </>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
 import { Box } from '@radix-ui/themes';
 import MovieList from './MovieList';
 
-const movies = [
-  { id: 1, title: 'Transformer 3', description: 'A long description...', price: 9.99 },
-  { id: 2, title: 'Lord of the Rings', description: 'A long description...', price: 9.99 },
-  { id: 3, title: 'Star Wars', description: 'A long description...', price: 9.99 },
-  { id: 4, title: 'Step Up 4', description: 'A long description...', price: 9.99 },
-  { id: 5, title: 'Cars', description: 'A long description...', price: 9.99 },
-  { id: 6, title: 'Moana', description: 'A long description...', price: 9.99 }
-]
-
 export default function Home() {
   return (
     <Box style={{ padding: 10 }}>
-      <MovieList movies={movies} />
+      <MovieList />
     </Box>
   );
 }
